3c4f95fbb0d8f5b76f148d03d393c2009ed7761e
unknown <yb825112@gmail.com>
2019-12-02 17:27:08 +0530

    Books Create/Index/New

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const bodyParder = require("body-parser");
 
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
+const bookRouter = require("./routes/books");
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -24,5 +25,6 @@ db.once('open', () => console.log('Connected to mongoose..'));
 
 app.use('/', indexRouter);
 app.use('/authors', authorRouter);
+app.use('/books', bookRouter);
 
 app.listen(process.env.PORT || 3000);
